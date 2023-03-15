e66b3a75c0f2bd1b0e1589afe93fd258ba44d031
RafaelDHenrique <rafaellamim99@gmail.com>
2023-03-14 22:38:46 -0300

    second commit

--- file-reader.py
+++ file-reader.py
@@ -1,3 +1,27 @@
+from collections import Counter
+
 class FileReader:
     def __init__(self, file):
         self.file = file
+
+    def read_file(self):
+        opened_file = open(self.file,'r', encoding= "UTF-8")
+        opened_file = opened_file.read()
+        print(opened_file)
+        print(type(opened_file))
+    
+    def take_refs(self):
+        pass
+
+    def take_values(self):
+        pass
+
+    def create_new_file(self):
+        pass
+
+
+
+
+
+first_try = FileReader('file.txt')
+first_try.read_file()
