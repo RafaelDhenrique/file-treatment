6a7f12d1ae2f5b565467f999be3f99c0788faa32
RafaelDHenrique <rafaellamim99@gmail.com>
2023-03-15 12:41:43 -0300

    extracting barcode from a list

--- file-reader.py
+++ file-reader.py
@@ -4,15 +4,21 @@ class FileReader:
     def __init__(self, file):
         self.file = file
 
+
     def read_file(self):
+        bar_code_list = []
         opened_file = open(self.file,'r', encoding= "UTF-8")
         opened_file = opened_file.read()
-        print(opened_file)
-        print(type(opened_file))
-    
-    def take_refs(self):
-        pass
+        for barcode in opened_file.split(','):
+            bar_code_list.append(barcode.split())
 
+        return bar_code_list
+    
+    def take_refs(self,index):
+        bar_code_list = self.read_file()
+        bar_code = ' '.join(bar_code_list[index])
+        print(type(bar_code))
+        
     def take_values(self):
         pass
 
@@ -24,4 +30,4 @@ class FileReader:
 
 
 first_try = FileReader('file.txt')
-first_try.read_file()
+first_try.take_refs(0)
